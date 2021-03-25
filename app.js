5ce83060532315e64279271ee8d5dcfbf05cbdbc
rocketgene <78985165+rocketgene@users.noreply.github.com>
2021-03-25 22:04:36 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+// setup express
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// route for static files
+app.use( '/static', express.static('public') );
+
+// import routes
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+// 404 handler
+app.use((req, res, next) => {
+    const err = new Error('Page not found.');
+    err.status = 404;
+    next(err);
+})
+
+// global error handler
+app.use((err, req, res, next) => {
+    if (err.status === 404) {
+        res.status(err.status);
+        res.render('page-not-found', {err});
+    } else {
+        res.status(500);
+        res.render('error', {err});
+    }
+});
+
+// setup server
+app.listen(3000);
+console.log('listeing on port:3000')
---
+++ routes/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const { projects } = require('../data.json');
+
+// route home page
+router.get('/', function(req, res, next) {
+    res.render('index', { projects });
+});
+
+// route about page
+router.get('/about', function(req, res, next) {
+    res.render('about', { projects });
+});
+
+// route recipes page (dynamic)
+router.get('/projects/:id', function(req, res, next) {
+    const projectId = req.params.id;
+    const project = projects.find( ({ id }) => id === +projectId );
+    
+    if (project) {
+      res.render('project', { project });
+    } else {
+        console.log('called else here')
+        const err = new Error();
+        err.status = 404;
+        err.message = `This project page does not exist`
+        next(err);
+    }
+});
+
+module.exports = router;
